5848d7eecd72a63118838b4c575f07687fd16d5c
avkotau <kotov.alexander1@mail.ru>
2023-06-24 17:47:32 +0200

    feat: add: SCSS, global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,45 @@
 import React from 'react';
 
-import s from './App.module.css';
+import s from './App.module.scss';
 
 function App() {
-  return (
-    <div className={s.app}>
-        <div className={s.leftSidebarContainer}>
-            <div className={s.meImg}>me</div>
-        </div>
-        <div className={s.mainContainer}>main
-            <div className={s.main}>
-                <div className={s.name}>
-                    <h1>Say Hi from Alex, framer Developer</h1>
+    return (
+        <div className={s.app}>
+            <div className={s.leftSidebarContainer}>
+                <div className={s.leftSidebar}>
+                    <div className={s.meImg}>me</div>
                 </div>
             </div>
-            <div className={s.aboutContainer}> about me</div>
-            <div className={s.resumeContainer}>Education & Experience</div>
-            <div className={s.skillsContainer}>My skills</div>
-            <div className={s.portfolioContainer}>Featured Projects</div>
-            <div className={s.contactContainer}>Contact</div>
-        </div>
-        <ul className={s.menuContainer}>menu</ul>
 
-    </div>
-  );
+            <div className={s.mainContainer}>
+                <div className={s.main}>
+                    <div className={s.name}>
+                        <h1>Say Hi from Alex, framer Developer</h1>
+                    </div>
+                </div>
+                <div className={s.aboutContainer}>
+                    <div className={s.about}>about me</div>
+                </div>
+                <div className={s.resumeContainer}>
+                    <div className={s.resume}>Education & Experience</div>
+                </div>
+                <div className={s.skillsContainer}>
+                    <div className={s.skills}>My skills</div>
+                </div>
+                <div className={s.portfolioContainer}>
+                    <div className={s.portfolio}>Featured Projects</div>
+                </div>
+                <div className={s.contactContainer}>
+                    <div className={s.contact}>Contact</div>
+                </div>
+            </div>
+
+            <div className={s.menuContainer}>
+                <ul className={s.menu}>menu</ul>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
